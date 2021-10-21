e01881021663a28079ccd04386addd97c303aeaa
krishnakanth16082001 <83630641+krishnakanth16082001@users.noreply.github.com>
2021-10-21 15:28:39 +0530

    Add files via upload

---
+++ kk/login.php
@@ -0,0 +1,36 @@
+﻿<?php
+	
+	$email = $_POST['email'];
+	$password = $_POST['password'];
+	$conn = new mysqli('localhost','root','','onine_passport');
+	if($conn->connect_error){
+		echo "$conn->connect_error";
+		die("Connection Failed : ". $conn->connect_error);
+	} else {
+		$stmt = $conn->prepare("select * from register where email= ?");
+		$stmt->bind_param("s", $email);
+		$stmt->execute();
+		$stmt_result = $stmt->get_result();
+		if($stmt_result->num_rows > 0 ){
+			$data = $stmt_result->fetch_assoc();
+            if ($data['pasword']==$password ) {
+            
+            	
+echo '<script language="Javascript">
+	alert("Logged In successfully ");
+	window.location=("pesonal2.php");</script>';
+            }
+            else{
+            	echo  '<script language="Javascript">
+	alert("Invalid Username Or Password ");
+	window.location=("login2.php");
+	</script>';
+            }
+        }
+else {
+	echo"invalid";
+}
+}
+
+		
+?>
---
+++ kk/login2.php
@@ -0,0 +1,26 @@
+﻿<html>
+<head>
+<style>
+.link{
+background-color:#450000;
+color:#b5b575;
+}
+</style>
+</head>
+<body bgcolor="#b5b575">
+<center>
+<form method="post" action="login.php">
+<fieldset>
+<legend><h1>TO  LOGIN</h1></legend>
+<p>
+<pre>
+
+Email	:	<input type="text" name="email" required size=30><br><br>
+Password	:	<input type ="password" name="password" required size=30><br>
+	<input type="submit" name="submit" value="SUBMIT">		<input type="reset" name="reset" value="RESET">
+
+</p>
+</form>
+</fieldset>
+</body>
+</html>
